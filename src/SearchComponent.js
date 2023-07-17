48d83218bda237c780c0031ccf2c2535768ee1e3
SPZN-org <79303958+SPZN-org@users.noreply.github.com>
2023-07-17 10:49:22 +0800

    Delete src directory

--- src/SearchComponent.js
+++
@@ -1,7 +0,0 @@
-import { data } from './Data';
-
-export function searchByTitle(searchText) {
-    const searchLowerCase = searchText.toLowerCase();
-    return data.filter(item => item.title.toLowerCase().includes(searchLowerCase))
-        .sort((a, b) => a.title.localeCompare(b.title));
-}
